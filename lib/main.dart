fb4621e6c9c77ee894d0d8d37e4a828087e7f761
3lVv0w <krseisenh@gmail.com>
2019-02-19 19:46:38 +0700

    add mqtt list subscribe

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,111 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:flutter/services.dart';
 
-final MqttClient client =
-    MqttClient('mqtt.cmmc.io', ''); //replace with your own mqtt
+final MqttClient client = MqttClient(
+    'mqtt.cmmc.io', 'clientId-DMzA9ut6aq'); //replace with your own mqtt
 
-const String pubTopic = ''; //replace with your own mqtt topic
+//replace with your own mqtt topic
+
+class Result {
+  final DeviceInfo deviceInfo;
+  final List<SensorData> records;
+
+  Result({this.deviceInfo, this.records});
+
+  factory Result.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['d'] as List;
+    List<SensorData> recordsList =
+        list.map((i) => SensorData.fromJson(i)).toList();
+
+    return Result(records: recordsList);
+  }
+}
+
+class DeviceInfo {
+  final String ssid;
+  final String chipId;
+  final int flashSize;
+  final String mac;
+  final String id;
+  final String clientId;
+  final String deviceId;
+  final String prefix;
+  final String ip;
+  final String version;
+
+  DeviceInfo(
+      {this.ssid,
+      this.chipId,
+      this.flashSize,
+      this.mac,
+      this.id,
+      this.clientId,
+      this.deviceId,
+      this.prefix,
+      this.ip,
+      this.version});
+
+  factory DeviceInfo.fromJson(Map<String, dynamic> parsedJson) {
+    return DeviceInfo(
+        ssid: parsedJson['info']['ssid'],
+        chipId: parsedJson['info']['chipId'],
+        flashSize: parsedJson['info']['flsahSize'],
+        mac: parsedJson['info']['mac'],
+        id: parsedJson['info']['id'],
+        clientId: parsedJson['info']['client_id'],
+        deviceId: parsedJson['info']['device_id'],
+        prefix: parsedJson['info']['prefix'],
+        ip: parsedJson['info']['ip'],
+        version: parsedJson['info']['version']);
+  }
+}
+
+class SensorData {
+  final String myName;
+  final int millis;
+  final int relayState;
+  final double temp2;
+  final double temp3;
+  final double temp4;
+  final int updateInterval;
+  final int heap;
+  final int rssi;
+  final int counter;
+  final int subscription;
+
+  SensorData(
+      {this.myName,
+      this.millis,
+      this.relayState,
+      this.temp2,
+      this.temp3,
+      this.temp4,
+      this.updateInterval,
+      this.heap,
+      this.rssi,
+      this.counter,
+      this.subscription});
+
+  factory SensorData.fromJson(Map<String, dynamic> parsedJson) {
+    return SensorData(
+        myName: parsedJson['myName'],
+        millis: parsedJson['millis'],
+        relayState: parsedJson['relayState'],
+        temp2: parsedJson['temp2'],
+        temp3: parsedJson['temp3'],
+        temp4: parsedJson['temp4'],
+        updateInterval: parsedJson['updateInterval'],
+        heap: parsedJson['heap'],
+        rssi: parsedJson['rssi'],
+        counter: parsedJson['counter'],
+        subscription: parsedJson['subscription']);
+  }
+}
 
 void main() => runApp(MyApp());
 
@@ -65,9 +163,10 @@ class _MyHomePageState extends State<MyHomePage> {
     client.onConnected = onConnected;
 
     final MqttConnectMessage connMess = MqttConnectMessage()
-        .withClientIdentifier('') //replace with your own mqtt
+        .withClientIdentifier(
+            'clientId-DMzA9ut6aq') //replace with your own mqtt
         .startClean()
-        .withWillQos(MqttQos.atLeastOnce);
+        .withWillQos(MqttQos.atMostOnce);
     print('CMMC::client connecting....');
     client.connectionMessage = connMess;
 
@@ -89,12 +188,12 @@ class _MyHomePageState extends State<MyHomePage> {
     return 0;
   }
 
-  Future<Null> _onPublishMessage(command) async {
+  Future<Null> _onPublishMessage(pubTopic, command) async {
     final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
     builder.addString(command);
 
     print('CMMC::Publishing our topic');
-    client.publishMessage(pubTopic, MqttQos.atLeastOnce, builder.payload);
+    client.publishMessage(pubTopic, MqttQos.atMostOnce, builder.payload);
 
     print('CMMC::Sleeping....');
     await MqttUtilities.asyncSleep(120);
@@ -108,6 +207,12 @@ class _MyHomePageState extends State<MyHomePage> {
     exit(-1);
   }
 
+  Future<Result> _onSubscribeMessage(pubTopic) async {
+    print('CMMC::Subscribing to the $pubTopic topic');
+    final message = client.subscribe(pubTopic, MqttQos.exactlyOnce);
+    return Result.fromJson(json.decode(message.changes.toString()));
+  }
+
   void onConnected() {
     print(
         'CMMC::OnConnected client callback - Client connection was sucessful');
@@ -115,36 +220,209 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _turnOn() {
     setState(() {
-      _onPublishMessage("ON");
+      _onPublishMessage("TECH_FEST/PLUG_005/\$/command", "ON");
       _state = "ON";
+      _onSubscribeMessage("TECH_FEST/Grill/status");
     });
   }
 
   void _turnOff() {
     setState(() {
-      _onPublishMessage("OFF");
+      _onPublishMessage("TECH_FEST/PLUG_005/\$/command", "OFF");
       _state = "OFF";
+      _onSubscribeMessage("TECH_FEST/Grill/status");
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // final key = GlobalKey<ScaffoldState>();
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'LED Status: ',
-              ),
-              Text(
-                '$_state',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
+          child: FutureBuilder<Result>(
+            future: _onSubscribeMessage("TECH_FEST/Grill/status"),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    padding: const EdgeInsets.all(16.0),
+                    itemCount: snapshot.data.records.length,
+                    itemBuilder: (context, index) {
+                      // Widget _buildImage() {
+                      //   if (snapshot.data.residents[index].imageLink.isEmpty) {
+                      //     return Container(
+                      //       child: Image.asset(
+                      //         'images/google_io_2018.jpg',
+                      //         fit: BoxFit.fitWidth,
+                      //       ),
+                      //     );
+                      //   } else {
+                      //     return Image.network(
+                      //         snapshot.data.residents[index].imageLink,
+                      //         fit: BoxFit.fitWidth);
+                      //   }
+                      // }
+
+                      return GestureDetector(
+                          //You need to make my child interactive
+                          // onTap: () {
+                          //   Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => DetailScreen(
+                          //             todo: snapshot.data.residents[index])),
+                          //   );
+                          // },
+                          // onLongPress: () {
+                          //   showDialog(
+                          //       context: context,
+                          //       builder: (BuildContext context) {
+                          //         return SimpleDialog(
+                          //           title: const Text('Would You like to do?'),
+                          //           children: <Widget>[
+                          //             SimpleDialogOption(
+                          //               onPressed: () {
+                          //                 Navigator.of(context,
+                          //                         rootNavigator: true)
+                          //                     .pop('dialog');
+                          //                 key.currentState
+                          //                     .showSnackBar(SnackBar(
+                          //                   content: Text('Added To Favorite'),
+                          //                 ));
+                          //               },
+                          //               child: const Text('Add To Favorite'),
+                          //             ),
+                          //             SimpleDialogOption(
+                          //               onPressed: () {
+                          //                 Navigator.of(context,
+                          //                         rootNavigator: true)
+                          //                     .pop('dialog');
+                          //                 Navigator.of(context)
+                          //                     .pushNamed('/chats');
+                          //               },
+                          //               child: const Text('Star Chat'),
+                          //             ),
+                          //           ],
+                          //         );
+                          //       });
+                          // },
+                          child: Card(
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.all(7.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                        padding: EdgeInsets.all(7.0),
+                                        child: Column(children: <Widget>[
+                                          Text(
+                                              snapshot
+                                                  .data.records[index].myName,
+                                              textAlign: TextAlign.justify,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: TextStyle(fontSize: 15.0))
+                                        ]))
+                                  ],
+                                )),
+                            // _buildImage(),
+                            Padding(
+                                padding: EdgeInsets.all(7.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Icon(Icons.attach_money),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Text(
+                                          'Month: ' +
+                                              snapshot.data.records[index].temp2
+                                                  .toString(),
+                                          style: TextStyle(fontSize: 15.0)),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Text(
+                                          'Daily: ' +
+                                              snapshot.data.records[index].temp3
+                                                  .toString(),
+                                          style: TextStyle(fontSize: 15.0)),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Text(
+                                          'Daily: ' +
+                                              snapshot.data.records[index].temp4
+                                                  .toString(),
+                                          style: TextStyle(fontSize: 15.0)),
+                                    )
+                                  ],
+                                )),
+                            Padding(
+                                padding: EdgeInsets.all(7.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Icon(Icons.access_time),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Text(
+                                          'Updated At: ' +
+                                              snapshot.data.records[index]
+                                                  .updateInterval
+                                                  .toString(),
+                                          style: TextStyle(fontSize: 15.0)),
+                                    )
+                                  ],
+                                ))
+                          ],
+                        ),
+                      ));
+                    });
+              } else if (snapshot.hasError) {
+                return ListView.builder(
+                    padding: const EdgeInsets.all(16.0),
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                      //You need to make my child interactive
+                      return Card(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                                padding: EdgeInsets.all(7.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Container(
+                                        padding: EdgeInsets.all(7.0),
+                                        child: Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              Text('Something Went Wrong :(',
+                                                  textAlign: TextAlign.center,
+                                                  overflow:
+                                                      TextOverflow.ellipsis,
+                                                  style:
+                                                      TextStyle(fontSize: 20.0))
+                                            ]))
+                                  ],
+                                ))
+                          ],
+                        ),
+                      );
+                    });
+              }
+              return CircularProgressIndicator();
+            },
           ),
         ),
         floatingActionButton: Column(
